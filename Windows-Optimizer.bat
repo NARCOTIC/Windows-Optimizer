a7fc3fbd42615cf053aa06ee8729c7389b6c8433
KID-X <58233243+kid-x@users.noreply.github.com>
2019-12-01 15:35:09 -0500

    Run as Administrator

---
+++ Windows-Optimizer.bat
@@ -0,0 +1,51 @@
+@echo off
+
+mode 84,25
+color 0A
+
+echo Windows Optimizer 1.0 ($) GITHUB/KID-X
+echo ------------------------------------------------------------------------------------
+
+ping 8.8.8.8 -n 4 > nul
+
+color 0B
+echo Optimizing and Defragmenting All Connected Storage Drives.
+echo.
+defrag /c /a /o /u
+ping 8.8.8.8 -n 4 > nul
+echo.
+
+color 0C
+Scanning and Fixing File System Errors.
+echo.
+chkdsk C: /f /r /scan
+ping 8.8.8.8 -n 4 > nul
+echo.
+
+color 0D
+Scanning and Fixing File System Errors Deeply.
+echo.
+sfc /scannow
+ping 8.8.8.8 -n 4 > nul
+
+color 0A
+echo.
+echo ------------------------------------------------------------------------------------
+echo Windows Optimizer 1.0 ($) GITHUB/KID-X
+echo.
+pause
+
+
+
+
+
+
+
+
+
+
+echo DISKS OPTIMIZED | GITHUB/KID-X
+echo ------------------------------------------------------------------------------------
+
+pause
+exit
